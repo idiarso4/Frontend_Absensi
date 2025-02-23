b43cc5a6f7d793b78ff8909d3132f833727277e0
idiarsosimbang@gmail.com <idiarsosimbang@gmail.com>
2025-02-23 10:20:12 +0700

    chore: update dependencies and configuration files

--- lib/main.dart
+++ lib/main.dart
@@ -2,47 +2,36 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:absen_smkn1_punggelan/app/presentation/login/login_screen.dart';
 import 'package:absen_smkn1_punggelan/app/presentation/login/login_notifier.dart';
-import 'package:absen_smkn1_punggelan/app/routes/app_routes.dart';
-import 'package:absen_smkn1_punggelan/app/presentation/dashboard/dashboard_notifier.dart';
-import 'package:absen_smkn1_punggelan/app/presentation/attendance/attendance_notifier.dart';
-import 'package:absen_smkn1_punggelan/app/presentation/attendance/history_notifier.dart';
+import 'package:absen_smkn1_punggelan/app/presentation/home/home_notifier.dart';
 import 'package:absen_smkn1_punggelan/app/presentation/attendance/schedule_notifier.dart';
-import 'package:absen_smkn1_punggelan/core/di/dependency.dart';
-import 'package:absen_smkn1_punggelan/core/widget/error_app_widget.dart';
-import 'package:absen_smkn1_punggelan/core/widget/loading_app_widget.dart';
-import 'package:intl/date_symbol_data_local.dart';
-import 'package:absen_smkn1_punggelan/core/helper/notification_helper.dart';
-import 'package:absen_smkn1_punggelan/app/presentation/leave/leave_notifier.dart';
-import 'package:absen_smkn1_punggelan/app/module/notifier/auth_notifier.dart';
-import 'package:absen_smkn1_punggelan/app/presentation/profile/profile_notifier.dart';
+import 'package:absen_smkn1_punggelan/app/core/helper/shared_preferences_helper.dart';
+import 'package:absen_smkn1_punggelan/app/di/injection.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await initializeDateFormatting('id', null);
-  await initDependency();
-  await NotificationHelper.initNotification();
-  runApp(MyApp());
+  await SharedPreferencesHelper.init();
+  await setupInjection();
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (_) => AuthNotifier()),
-        ChangeNotifierProvider(create: (_) => LoginNotifier()),
-        ChangeNotifierProvider(create: (_) => DashboardNotifier()),
-        ChangeNotifierProvider(create: (_) => AttendanceNotifier()),
-        ChangeNotifierProvider(create: (_) => HistoryNotifier()),
-        ChangeNotifierProvider(create: (_) => ScheduleNotifier()),
-        ChangeNotifierProvider(create: (_) => LeaveNotifier()),
-        ChangeNotifierProvider(create: (_) => ProfileNotifier()),
+        ChangeNotifierProvider(create: (_) => getIt<LoginNotifier>()),
+        ChangeNotifierProvider(create: (_) => getIt<HomeNotifier>()),
+        ChangeNotifierProvider(create: (_) => getIt<ScheduleNotifier>()),
       ],
       child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.red),
-        initialRoute: AppRoutes.login,
-        routes: AppRoutes.routes,
+        title: 'Absen SMKN 1 Punggelan',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+          useMaterial3: true,
+        ),
+        home: const LoginScreen(),
       ),
     );
   }
